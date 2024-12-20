fccd0db4f402ddcd19054dceb595ed8f04acb60e
lavantien <lavantien96@gmail.com>
2024-12-20 07:23:42 +0700

    add(model): qwen 2.5 coder 32b instruct

--- llama_bootstrap/qwen2.5-coder-32b-instruct-q5_k_m.ps1
+++ llama_bootstrap/qwen2.5-coder-32b-instruct-q5_k_m.ps1
@@ -1,15 +1,18 @@
 # Define the model path and parameters
-$modelPath = "C:\Users\lavantien\.cache\lm-studio\models\lmstudio-community\DeepSeek-Coder-V2-Lite-Instruct-GGUF\DeepSeek-Coder-V2-Lite-Instruct-Q6_K.gguf"
+$modelPath = "C:\Users\lavantien\.cache\lm-studio\models\Qwen\Qwen2.5-Coder-32B-Instruct-GGUF\qwen2.5-coder-32b-instruct-q5_k_m-00001-of-00003.gguf"
 
 # Define parameters as an object for readability
 $params = @{
-    "gpu-layers" = 8
+    "gpu-layers" = 18
     "ctx-size" = 32768
     "batch-size" = 512
     "threads" = 8
     "keep" = 4096
     "predict" = -1
+    "flash-attn" = $true
     "mlock" = $true
+    "cache-type-k" = "q8_0"
+    "cache-type-v" = "q8_0"
 }
 
 # Build the llama-server command with the specified parameters
@@ -34,5 +37,5 @@ foreach ($key in $params.Keys)
 }
 
 # Run the command
-Start-Process -FilePath "pwsh" -ArgumentList "-Command $cmd --no-context-shift" -NoNewWindow -Wait
+Start-Process -FilePath "pwsh" -ArgumentList "-Command $cmd" -NoNewWindow -Wait
 
